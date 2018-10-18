c8c2432338aa369b3ee87ba2d9b8f6a1cff06d53
Rakan Alghamdi <43054565+RakanAlg@users.noreply.github.com>
2018-10-17 21:56:57 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,30 +1,32 @@
-var request = new XMLHttpRequest();
-request.open('GET', 'https://jsonplaceholder.typicode.com/users/');
-request.onload = function () {
-  var data = JSON.parse(this.response);
-var copy=[];
-  data.forEach(user => {
-  copy.push(user.email);
-});
-copy.sort();
-var email = document.getElementById('demo');
-email.innerHTML=copy.join(" ");
-console.log(copy);
-
-}
-request.send();
-
-fetch('https://jsonplaceholder.typicode.com/users/').then(response => {
-  return response.json();
-}).then(data => {
-  var temp=[];
-  data.forEach(user => {
-    temp.push(user.name);
-});
-var name = document.getElementById('username');
-name.innerHTML=temp.sort(function(a, b){
-  return a.length - b.length;
-});
-}).catch(err => {
-
+var size=30;
+document.addEventListener('keyup', function (event) { 
+const code = event.keyCode;
+    
+    if(code==107)
+        {
+            if(size <60){
+            size=size+15;
+                document.getElementById('demo').innerHTML="&#x1F388;";
+            document.getElementById('demo').style.fontSize = (size)+"px";
+            }
+            else{
+            document.getElementById('demo').innerHTML="&#x1F5EF";
+        }}
+    else if(code==109)
+        {
+            if(size>0){
+                document.getElementById('demo').innerHTML="&#x1F388;";
+             size=size-15;
+            document.getElementById('demo').style.fontSize = (size)+"px";
+            }
+            else{
+                document.getElementById('demo').style.fontSize = '30px';
+            document.getElementById('demo').innerHTML="Done";
+            }
+        }
+    else{
+        
+  alert('keydown event\n\n' + 'Invalid key...! Please press + or - key');
+        
+    }
 });
